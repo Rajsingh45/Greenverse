84322992f6aec7772ebf5e1dec2243a385a77559
Sahil <singhsahil140404@gmail.com>
2024-06-26 17:42:10 +0530

    Changes

--- frontend/src/SignupPage.js
+++ frontend/src/SignupPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './login.css';
+import './signup.css';
 import { UserContext } from "./UserContext"; // Ensure this path is correct
 // import SignupPage from './LoginPage';
 import { FaUserCircle } from "react-icons/fa";
@@ -22,8 +22,8 @@ const SignupPage = () => {
         }));
     };
 
-    const handleSign=()=>{
-        navigate("/signup")
+    const handleLogin=()=>{
+        navigate("/")
     }
 
     const handleSubmit = (e) => {
@@ -78,33 +78,52 @@ const SignupPage = () => {
 
     return (
         <div className="container">
-            <div className="left-panel">
-                <div className="logo">
-                    <img src="/path/to/logo.png" alt="GreenVerse Logo" />
-                </div>
-                <h1 className='title'>GreenVerse</h1>
-                <p className='personal-details'>Enter your personal details to start the journey with us</p>
-                <button className="signup-btn" onClick={handleSign}>SIGN UP</button>
-            </div>
-            <div className="right-panel">
-                <div className="help-link">Need Help?</div>
-                <div className="form-container">
-                    <div className="avatar">
-                        <FaUserCircle className="avatar-icon" />
+            <div className="left-panel-new">
+            <div className="help-link-new">Need Help?</div>
+                <h1 className='account'>Create Account</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            value={userDetails.name}
+                            onChange={handleInput}
+                            required
+                        />
                     </div>
-                    <form onSubmit={handleSubmit}>
-                        <div className="input-group">
-                            <input type="email" name="email" placeholder="Email" value={userDetails.email} onChange={handleInput} />
-                        </div>
-                        <div className="input-group">
-                            <input type="password" name="password" placeholder="Password" value={userDetails.password} onChange={handleInput} />
-                        <button type="submit" className="signin-btn">SIGN IN</button>
-                        </div>
-                    </form>
-                </div>
+                    <div className="input-group">
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            value={userDetails.email}
+                            onChange={handleInput}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            value={userDetails.password}
+                            onChange={handleInput}
+                            required
+                        />
+                        <button type="submit" className="signup-btn">SIGN UP</button>
+                    </div>
+                    
+                </form>
+            </div>
+            <div className="right-panel-new">
+                <h1 className='welcome'>Welcome Back!</h1>
+                <p className='connected'>To keep connected with us please login with your personal info</p>
+                <button className="signin-btn" onClick={handleLogin}>SIGN IN</button>
             </div>
         </div>
     );
+
 }
 
 export default SignupPage;
