cfd7a765604c61eb437847068fc228643e758a7b
Sahil <singhsahil140404@gmail.com>
2024-07-08 20:36:31 +0530

    NA

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+
 const authRoutes = require('./routes/auth');
 const aqiRoutes = require('./routes/aqi');
 const adminRoutes = require('./routes/admin'); // Add this line
