ab3657b0ebb0cf8f57aa401beb0a07506f4d8b9f
Sahil <singhsahil140404@gmail.com>
2024-07-12 18:30:59 +0530

    Modified the UI

--- frontend/src/UserContext.js
+++ frontend/src/UserContext.js
@@ -4,9 +4,16 @@ export const UserContext = createContext();
 
 const UserContextProvider = ({ children }) => {
     const [loggedUser, setLoggedUser] = useState(null);
+    const [users, setUsers] = useState([]);
+
+    const handleUserUpdated = (updatedUser) => {
+        setUsers((prevUsers) =>
+          prevUsers.map((user) => (user._id === updatedUser._id ? updatedUser : user))
+        );
+    };
 
     return (
-        <UserContext.Provider value={{ loggedUser, setLoggedUser }}>
+        <UserContext.Provider value={{ loggedUser, setLoggedUser, users, handleUserUpdated }}>
             {children}
         </UserContext.Provider>
     );
