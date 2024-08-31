0b68bba3acada1f6317e9e4dd30a280f6b25b5a6
Raj Singh <rajsingh30930@gmail.com>
2024-08-31 22:13:48 +0530

    semicolon

--- frontend/src/UserDashboard/DeviceDetail.js
+++ frontend/src/UserDashboard/DeviceDetail.js
@@ -11,7 +11,7 @@ import IconButton from '@mui/material/IconButton';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
-const backendURL= "https://greenverse.onrender.com";
+const backendURL= "https://greenverse.onrender.com"
 
 const DeviceDetailPage = () => {
     const [downloadStartDate, setDownloadStartDate] = useState(new Date());
