cdb3254874c06fd08f896752c8a25b341998ab90
Sahil <singhsahil140404@gmail.com>
2024-09-03 10:40:43 +0530

    Undo

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 // Ensure the uploads folder is created
 const uploadsFolderPath = path.join(__dirname, '..', 'uploads');
-// if (!fs.existsSync(uploadsFolderPath)) {
-//   fs.mkdirSync(uploadsFolderPath, { recursive: true });
-//   console.log('Uploads folder created.');
-// }
+if (!fs.existsSync(uploadsFolderPath)) {
+  fs.mkdirSync(uploadsFolderPath, { recursive: true });
+  console.log('Uploads folder created.');
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
